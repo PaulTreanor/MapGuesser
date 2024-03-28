5fa9c8ed48d0f0c29d8ea6f32358dcd51fea50b9
paultreanor <treanorpaul9@gmail.com>
2024-03-28 17:25:35 +0000

    Add map image and replace package-lock with bun

--- frontend-service/src/pages/index.tsx
+++ frontend-service/src/pages/index.tsx
@@ -1,11 +1,13 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
+import Map from "../images/irelandMap.svg"
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <main>
       <h1 className="text-amber-700">MapGusser</h1>
       <p>Guess the location of a photo on a map!</p>
+      <img src={Map} alt="Ireland map" />
     </main>
   )
 }
