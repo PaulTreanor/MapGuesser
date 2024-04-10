b9337e21bd30f6d066904e4cce590f658b3e36e2
paultreanor <treanorpaul9@gmail.com>
2024-04-10 22:33:45 +0100

    Add basic line between pins code

--- frontend-service/src/components/MapBoxMap.tsx
+++ frontend-service/src/components/MapBoxMap.tsx
@@ -5,6 +5,7 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 const MapboxMap = () => {
   const mapContainerRef = useRef(null);
   const [lastClick, setLastClick] = useState({ lng: 0, lat: 0 });
+  const parisCoords = [2.3488, 48.8534]
 
     useEffect(() => {
       mapboxgl.accessToken = "pk.eyJ1IjoicGF1bHRyZWFub3IiLCJhIjoiY2x1dTk2MGZ6MDd5MTJrc3RheHl6ZGE1cCJ9.i2IJpqtnJjbclBOLbaVnXw";
@@ -24,7 +25,42 @@ const MapboxMap = () => {
           // Create a new marker and add it to the map at the clicked location
           new mapboxgl.Marker()
               .setLngLat([e.lngLat.lng, e.lngLat.lat])
-              .addTo(map);
+            .addTo(map);
+          
+
+          new mapboxgl.Marker({ color: "red" })
+            .setLngLat(parisCoords)
+            .addTo(map);
+
+          const lineCoordinates = [
+            [e.lngLat.lng, e.lngLat.lat], // Clicked location
+            parisCoords // Paris coordinates
+          ];
+
+          // Create a GeoJSON source with a line feature
+          map.addSource('line', {
+            'type': 'geojson',
+            'data': {
+              'type': 'Feature',
+              'properties': {},
+              'geometry': {
+                'type': 'LineString',
+                'coordinates': lineCoordinates
+              }
+            }
+          });
+
+          // Add a new layer to visualize the line
+          map.addLayer({
+            'id': 'line',
+            'type': 'line',
+            'source': 'line',
+            'layout': {},
+            'paint': {
+              'line-width': 2,
+              'line-color': '#007cbf'
+            }
+          });
 
           // Update state with the clicked coordinates
           setLastClick(e.lngLat);
