547136be6c08938fcacdfadd298f96236e4a9ba3
paultreanor <treanorpaul9@gmail.com>
2024-04-10 22:04:52 +0100

    Add code for putting pins down on map

--- frontend-service/src/components/MapBoxMap.tsx
+++ frontend-service/src/components/MapBoxMap.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 const MapboxMap = () => {
-    const mapContainerRef = useRef(null);
+  const mapContainerRef = useRef(null);
+  const [lastClick, setLastClick] = useState({ lng: 0, lat: 0 });
 
     useEffect(() => {
       mapboxgl.accessToken = "pk.eyJ1IjoicGF1bHRyZWFub3IiLCJhIjoiY2x1dTk2MGZ6MDd5MTJrc3RheHl6ZGE1cCJ9.i2IJpqtnJjbclBOLbaVnXw";
@@ -15,24 +16,23 @@ const MapboxMap = () => {
             // center: [10, 47], // Starting position [lng, lat] - not used with custom style
             // zoom: 3 // Starting zoom
         });
+      
+        const addMarker = (e) => {
+          // Remove existing markers
+          document.querySelectorAll('.mapboxgl-marker').forEach(marker => marker.remove());
 
-        map.on('load', () => {
-            map.addSource('countries', {
-                type: 'vector',
-                url: 'mapbox://mapbox.mapbox-countries-v1'
-            });
-            map.addLayer({
-                'id': 'countries-layer',
-                'type': 'fill',
-                'source': 'countries',
-                'source-layer': 'country_boundaries',
-                'paint': {
-                    'fill-color': '#2276AC',
-                    'fill-opacity': 0.5
-                },
-                'filter': ['==', ['get', 'disputed'], 'false']
-            });
-        });
+          // Create a new marker and add it to the map at the clicked location
+          new mapboxgl.Marker()
+              .setLngLat([e.lngLat.lng, e.lngLat.lat])
+              .addTo(map);
+
+          // Update state with the clicked coordinates
+          setLastClick(e.lngLat);
+      };
+
+        map.on('click', addMarker);
+ 
+       
 
         // Clean up on unmount
         return () => map.remove();
