26ea922a64dddbc5ada8e0e92caf151d7f1df06d
paultreanor <treanorpaul9@gmail.com>
2024-04-13 14:39:14 +0100

    Make game usable on mobile

--- frontend-service/src/components/TopBarGame.tsx
+++ frontend-service/src/components/TopBarGame.tsx
@@ -7,25 +7,26 @@ interface TopBarGameProps {
   moveToNextRound: () => void
 }
 
+
 export default function TopBarGame({ roundLocation, score, currentRound, roundCompleted, moveToNextRound}) {
   return (
     <nav className="border-gray-200 pointer-events-none min-h-64">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-4 pointer-events-auto">
-        <div className="flex items-center">
-          <h1 className="text-amber-700 text-3xl font-bold z-30 mr-4">MapGuesser</h1>
+      <div className="max-w-screen-xl flex flex-col md:flex-row md:flex-wrap items-center justify-between mx-auto py-4 pointer-events-auto">
+        <div className="flex flex-col md:flex-row items-center">
+          <h1 className="text-amber-700 text-3xl font-bold z-30 mr-4 hidden sm:block">MapGuesser</h1>
           <div className="p-4 bg-white rounded-md z-30"> 
             <h2 className='text-2xl'>Where is <span className='font-bold'>{roundLocation}</span>?</h2>
           </div>
         </div>
         <button
           hidden={!roundCompleted}
-          className='disabled:bg-gray-500 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded pointer-events-auto z-30'
+          className='disabled:bg-gray-500 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded pointer-events-auto z-30 mt-4 md:mt-0'
           onClick={moveToNextRound}
           disabled={!roundCompleted}
         >
           Next Round
         </button>
-        <div className='px-4 py-2 bg-white rounded-md z-30'>
+        <div className='px-4 py-2 bg-white rounded-md z-30 mt-4 md:mt-0 absolute bottom-0 right-0 mb-4 mr-4 md:relative md:mb-0 md:mr-0'>
           <p className='z-30'>{score} points</p>
           <p className='z-30'>{currentRound}/5</p>
         </div>
