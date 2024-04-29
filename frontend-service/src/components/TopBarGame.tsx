96775b78d198001ef0a6f365f84eed2a3e82ebce
paultreanor <treanorpaul9@gmail.com>
2024-04-29 08:50:30 +0100

    Improve button layouts for desktop and mobile

--- frontend-service/src/components/TopBarGame.tsx
+++ frontend-service/src/components/TopBarGame.tsx
@@ -11,25 +11,27 @@ interface TopBarGameProps {
 export default function TopBarGame({ roundLocation, score, currentRound, roundCompleted, moveToNextRound}: TopBarGameProps) {
   return (
     <nav className="border-gray-200 pointer-events-none min-h-64">
-      <div className="max-w-screen-xl flex flex-col md:flex-row md:flex-wrap items-center justify-between mx-auto py-4 pointer-events-auto">
-        <div className="flex flex-col md:flex-row items-center">
-          <h1 className="font-titillium text-blue-800 text-4xl font-bold z-30 mr-4 hidden sm:block">
+      <div className="mx-4 flex flex-col sm:flex-row sm:flex-wrap items-center justify-between py-4 pointer-events-auto">
+        <div className="flex flex-col sm:flex-row items-center">
+          <h1 className="font-titillium text-blue-800 text-4xl font-bold z-30 mr-4 hidden md:block md:absolute md:bottom-0 md:left-0 md:ml-4 md:mb-4 ">
             🌎
             <span className='text-shadow'> MapGuesser</span>
           </h1>
-          <div className="p-4 bg-white rounded-md z-30"> 
-            <h2 className='text-2xl'>Where is <span className='font-bold'>{roundLocation}</span>?</h2>
+          <div className="p-4 bg-blue-900 rounded-md z-30 shadow-slate-50 shadow-sm"> 
+            <h2 className='text-2xl text-white'>
+              Where is <span className='font-bold'>{roundLocation}</span>?
+            </h2>
           </div>
         </div>
         <button
           hidden={!roundCompleted}
-          className='disabled:bg-gray-500 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded pointer-events-auto z-30 mt-4 md:mt-0'
+          className='disabled:bg-gray-500 bg-rose-700 hover:bg-rose-800 text-white font-bold py-2 px-4 rounded pointer-events-auto z-30 mt-4 sm:mt-0 sm:absolute sm:bottom-0 sm:left-1/2 sm:transform sm:-translate-x-1/2 sm:mb-4  shadow-slate-50 shadow-sm'
           onClick={moveToNextRound}
           disabled={!roundCompleted}
         >
           Next Round
         </button>
-        <div className='px-4 py-2 bg-white rounded-md z-30 mt-4 md:mt-0 absolute bottom-0 right-0 mb-4 mr-4 md:relative md:mb-0 md:mr-0'>
+        <div className='px-4 py-2 bg-blue-900 text-white rounded-md z-30 mt-4 sm:mt-0 absolute bottom-0 right-0 mb-6 mr-4 sm:relative sm:mb-0 sm:mr-0  shadow-slate-50 shadow-sm'>
           <p className='z-30'>{score} points</p>
           <p className='z-30'>{currentRound}/5</p>
         </div>
