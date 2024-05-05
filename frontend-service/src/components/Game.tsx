54ef51e02e413b1bea9c6994c1229f7c7d195c32
paultreanor <treanorpaul9@gmail.com>
2024-05-05 02:15:55 +0100

    Fix modal appearance on small devices

--- frontend-service/src/components/Game.tsx
+++ frontend-service/src/components/Game.tsx
@@ -66,10 +66,10 @@ export default function Game() {
       { isModalOpen && 
         <Modal onClose={() => setIsModalOpen(false)}>
           <h1
-            className="font-titillium text-blue-800 text-4xl font-bold mb-4"
+            className="font-titillium text-blue-800 text-4xl font-bold md:mb-2 flex flex-col md:flex-row items-baseline"
           >
             🌎 MapGuesser 
-          <span className="text-slate-400 text-xl font-light ml-2">  Inspired by GeoGuessr!</span>
+            <span className="text-slate-400 text-xl font-light mt-2 md:mt-0 md:ml-2">Inspired by GeoGuessr!</span>
           </h1>
           <br />
           <p className="text-lg text-slate-950">
