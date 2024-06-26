67320742f434ea3accb8c362f33f664b718364d2
paultreanor <treanorpaul9@gmail.com>
2024-06-26 09:17:06 +0100

    Improve distance accuracy, tests, and README
    - Used turf.distance() to calculate km distance instead of rolling my
      own algorithm. This is way more accurate and also simpler code.
    - Tests for distance are now actually passing which means I can
      uncomment them
    - Add testing and node version information to README
    - Remove unused test

--- frontend-service/README.md
+++ frontend-service/README.md
@@ -4,10 +4,17 @@
 - Hosted by Vercel
 
 ## Installation and setup
+I am using `Node v20.11.1` with this project.
+
 ```bash
 npm install
 npm run dev
 # local server running on http://localhost:8000
+```
+
+## Testing 
+```
+npx vitest
 ```
 
 ## Tech 
--- frontend-service/src/utils/mapUtils.ts
+++ frontend-service/src/utils/mapUtils.ts
@@ -1,19 +1,9 @@
 import { Pin } from '../Types';
+import { distance } from '@turf/distance';
 
 export function calculateKm(pin1: Pin, pin2: Pin): number {
-  const toRad = (value: number): number => (value * Math.PI) / 180;
-  const earthRadiusKm = 6371;
-
-  const dLat = toRad(pin2[0] - pin1[0]);
-  const dLon = toRad(pin2[1] - pin1[1]);
-
-  const lat1 = toRad(pin1[0]);
-  const lat2 = toRad(pin2[0]);
-
-  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-            Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2); 
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a)); 
-  return parseFloat((earthRadiusKm * c).toFixed(0));
+  const distanceInKm = distance(pin1, pin2, {units: 'kilometers'});
+  return Math.round(distanceInKm);
 }
 
 export function emojiForDistances(distance: number): string {
