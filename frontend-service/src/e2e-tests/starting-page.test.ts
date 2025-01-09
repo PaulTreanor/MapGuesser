c39d8ef79bc73b9f50f2f143f9eb229e0c6708d0
paultreanor <treanorpaul9@gmail.com>
2025-01-09 23:40:52 +0000

    Add e2e test to run through entire game

--- frontend-service/src/e2e-tests/starting-page.test.ts
+++ frontend-service/src/e2e-tests/starting-page.test.ts
@@ -6,3 +6,10 @@ test('has MapGuesser title', async ({ page }) => {
 	await expect(page).toHaveTitle('MapGuesser');
 });
 
+test('start game button renders', async ({ page }) => {
+	await page.goto('http://localhost:8000/');
+
+	const startGameButton = await page.getByRole('button', { name: 'Start Game!' });
+	await expect(startGameButton).toBeVisible();
+});
+
