e56db3c2684a62451550e5262e9e1bcc86a303d5
paultreanor <treanorpaul9@gmail.com>
2025-05-24 14:16:54 +0100

    Fix modal styling for mobile

--- frontend-service/src/components/Modal.tsx
+++ frontend-service/src/components/Modal.tsx
@@ -6,8 +6,8 @@ interface ModalProps {
 
 const Modal: React.FC<ModalProps> = ({ children }) => {
 	return (
-		<div className="fixed inset-0 bg-slate-900 bg-opacity-50 z-50 flex justify-center items-center">
-			<div className="bg-slate-100 p-4 md:p-8 rounded-lg shadow-lg max-w-2xl w-auto mx-4 sm:mx-2">
+		<div className="fixed inset-0 bg-slate-900 bg-opacity-50 z-50 flex justify-center items-start sm:items-center overflow-y-auto">
+			<div className="bg-slate-100 p-3 md:p-8 rounded-lg shadow-lg max-w-2xl w-auto mx-4 sm:mx-2 my-4 sm:my-0 max-h-screen overflow-y-auto">
 				{children}
 			</div>
 		</div>
