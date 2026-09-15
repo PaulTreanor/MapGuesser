4cb0f71d49733542ff8a023ecc3dbe0954ad4010
paultreanor <treanorpaul9@gmail.com>
2026-09-15 17:53:54 +0100

    Remove inspired by GeoGasser Message

--- frontend-service/src/components/typography/MapGuesserHeading.tsx
+++ frontend-service/src/components/typography/MapGuesserHeading.tsx
@@ -6,7 +6,6 @@ const MapGuesserHeading = () => {
             className="font-titillium text-blue-800 text-4xl font-bold md:mb-2 flex flex-col md:flex-row items-baseline"
         >
             🌎 MapGuesser 
-            <span className="text-slate-400 text-xl font-light mt-2 md:mt-0 md:ml-2">Inspired by GeoGuessr!</span>
         </h1>
     )
 }
